20c238e3d857d34b7c7709649c577de08f5c9fa4
Dũng <dungnt@vivas.vn>
2017-08-19 09:31:37 +0700

    update

--- MikrotikAPITypes.cpp
+++ MikrotikAPITypes.cpp
@@ -15,38 +15,38 @@ using namespace std;
  ********************************************************************/
 bool Sentence::Print()
 {
-	DEBUG ? printf("Sentence Word Count = %d\n", strWords.size()) : 0;
-	DEBUG ? printf("Sentence returnType = %d\n", returnType) : 0;
-	
-	for (int i = 0; i < strWords.size(); ++i) {
-		printf("%s\n", strWords[i].c_str());
-	}
-	
-	printf("\n");
-	
-	return true;
+    DEBUG ? printf("Sentence Word Count = %d\n", strWords.size()) : 0;
+    DEBUG ? printf("Sentence returnType = %d\n", returnType) : 0;
+
+    for (int i = 0; i < strWords.size(); ++i) {
+        printf("%s\n", strWords[i].c_str());
+    }
+
+    printf("\n");
+
+    return true;
 }
 
 void Sentence::GetMap(map<string, string> &sentenceMap)
 {
-	for (int i = 0; i < strWords.size(); ++i) {
-		string tmpDataString = strWords[i];
-		vector<string> dataStrings;
-		Tokenize(tmpDataString, dataStrings, "=");
+    for (int i = 0; i < strWords.size(); ++i) {
+        string tmpDataString = strWords[i];
+        vector<string> dataStrings;
+        Tokenize(tmpDataString, dataStrings, "=");
 
-		if (returnType == NONE && dataStrings.size() > 1) {
-			sentenceMap.insert(make_pair(dataStrings[1], dataStrings[2]));
-		}
-	}
+        if (returnType == NONE && dataStrings.size() > 1) {
+            sentenceMap.insert(make_pair(dataStrings[1], dataStrings[2]));
+        }
+    }
 }
 
 void Sentence::Tokenize(const string &str, vector<string> &tokens, const string &delimiters)
 {
-	// Skip delimiters at beginning.
+    // Skip delimiters at beginning.
     string::size_type lastPos = str.find_first_not_of(delimiters, 0);
     // Find first "non-delimiter".
     string::size_type pos     = str.find_first_of(delimiters, lastPos);
-	
+
     while (string::npos != pos || string::npos != lastPos)
     {
         // Found a token, add it to the vector.
@@ -62,10 +62,10 @@ void Sentence::Tokenize(const string &str, vector<string> &tokens, const string 
  * Add a Sentence to a block
  ********************************************************************/
 void Block::AddSentence(const Sentence &sentence)
-{	
-	sentences.push_back(sentence);
-	
-	DEBUG ? printf("AddSentenceToBlock Size=%d\n", sentences.size()) : 0;
+{
+    sentences.push_back(sentence);
+
+    DEBUG ? printf("AddSentenceToBlock Size=%d\n", sentences.size()) : 0;
 }
 
 /********************************************************************
@@ -73,12 +73,12 @@ void Block::AddSentence(const Sentence &sentence)
  ********************************************************************/
 bool Block::Print()
 {
-	DEBUG ? printf("PrintBlock\n") : 0;
-	DEBUG ? printf("Block Size = %d\n", sentences.size()) : 0;
-	
-	for (int i = 0; i < sentences.size(); ++i) {
-		sentences[i].Print();
-	}
-	
-	return true;
+    DEBUG ? printf("PrintBlock\n") : 0;
+    DEBUG ? printf("Block Size = %d\n", sentences.size()) : 0;
+
+    for (int i = 0; i < sentences.size(); ++i) {
+        sentences[i].Print();
+    }
+
+    return true;
 }
